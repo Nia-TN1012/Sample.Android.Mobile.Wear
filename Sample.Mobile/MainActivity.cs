58f9d971937afee23ef9e7728dd023927e599a8d
Nia-TN1012 <nia1012-tmnk@outlook.jp>
2018-02-27 18:28:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample.Mobile/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Sample.Mobile {
+	[Activity( Label = "Sample.Mobile", MainLauncher = true )]
+	public class MainActivity : Activity {
+		protected override void OnCreate( Bundle savedInstanceState ) {
+			base.OnCreate( savedInstanceState );
+
+			// Set our view from the "main" layout resource
+			SetContentView( Resource.Layout.Main );
+
+			var message = SampleShared.Greeting;
+		}
+	}
+}
+
